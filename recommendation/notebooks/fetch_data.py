784126f4b37ffef28bf0d0fba5cc26244ae461dd
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2022-06-14 12:09:23 +0545

    added rec handler

--- recommendation/notebooks/fetch_data.py
+++ recommendation/notebooks/fetch_data.py
@@ -1,48 +1,57 @@
 import sys
 import authorization
-import pandas as pd 
-from tqdm import tqdm  
+import pandas as pd
+from tqdm import tqdm
 import time
 
-#GET AUTH
+# Authorize and call access object "sp"
 sp = authorization.authorize()
 
-#GET ALL GENRES 
+# Get all genres
 genres = sp.recommendation_genre_seeds()
 
-#NO. OF RECOMMENDATIONS PER GENRE 
-n_recs = 100 
+# Set number of recommendations per genre
+n_recs = 100
 
-#information to crawl
-data_dict = {"id":[], "genre":[], "track_name":[], "artist_name":[],"valence":[], "energy":[]}
+# Initiate a dictionairy with all the information you want to crawl
+data_dict = {"id":[], "genre":[], "track_name":[], "artist_name":[],
+             "valence":[], "energy":[]}
 
-#Crawling 
+################
+## CRAWL DATA ##
+################
+
+# Get recs for every genre
 for g in tqdm(genres):
-    #get n recommendation 
-    recs = sp.recommendations(genres=[g], limit = n_recs)
-    recs = eval(recs.json().replace("null","-999").replace("false","False").replace("true","True"))["tracks"]
-
+    
+    # Get n recommendations
+    recs = sp.recommendations(genres = [g], limit = n_recs)
+    # json-like string to dict
+    recs = eval(recs.json().replace("null", "-999").replace("false", "False").replace("true", "True"))["tracks"]
+    # Crawl data from each track
     for track in recs:
-        # ID AND GENRE
+        # ID and Genre
         data_dict["id"].append(track["id"])
         data_dict["genre"].append(g)
-
+        # Metadata
         track_meta = sp.track(track["id"])
         data_dict["track_name"].append(track_meta.name)
         data_dict["artist_name"].append(track_meta.album.artists[0].name)
-
-        #VALENCE AND ENERGY
+        # Valence and energy
         track_features = sp.track_audio_features(track["id"])
         data_dict["valence"].append(track_features.valence)
         data_dict["energy"].append(track_features.energy)
-
+        
+        # Wait 0.2 seconds per track so that the api doesnt overheat
         time.sleep(0.2)
+        
+##################
+## PROCESS DATA ##
+##################
 
-
-#PROCESS DATA 
-
-#Store data in dataframe
+# Store data in dataframe
 df = pd.DataFrame(data_dict)
 
-df.drop_duplicates(subset = 'id', keep='first', inplace=True)
+# Drop duplicates
+df.drop_duplicates(subset = "id", keep = "first", inplace = True)
 df.to_csv("valence_arousal_dataset.csv", index = False)
