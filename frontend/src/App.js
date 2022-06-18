dccc4534e67ffa088c3e12ef618a1d91158fe2bd
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2022-06-18 11:27:34 +0545

    start frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Play from './components/Play/Play';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Navbar />
-      </header>
-      <div className='content'>
-        <h1>Content</h1>
+      <div className="App">
+        
       </div>
-      <div className='player'>
-        <Play />
-      </div>
-    </div>
   );
 }
 
