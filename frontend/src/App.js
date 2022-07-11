718f1b643786f3a3eed82ee96961e7bb57d28656
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2022-07-11 13:11:38 +0545

    home page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,70 @@
-import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Library from './components/library/Library'
-import Recommendation from './components/recommendation/Recommendation'
-import Search from './components/search/Search'
-import User from './components/user/User'
-import Home from './components/Home/Home'
+import { useEffect, useState } from "react";
+import Library from "./components/library/Library";
+import Recommendation from "./components/recommendation/Recommendation";
+import Search from "./components/search/Search";
+import User from "./components/user/User";
+import Home from "./components/Home/Home";
 
 function App() {
+  const CLIENT_ID = "12824720980943c684531a5514a21114";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path ='/' element={<Home />} />
-            <Route path ='/search' element={<Search />} />
-            <Route path ='/library' element={<Library />} />
-            <Route path ='/recommendation' element={<Recommendation />} />
-            <Route path ='/user' element={<User />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+    <div className="App">
+      <a
+        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+      >
+        Login to Spotify
+      </a>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/recommendation" element={<Recommendation />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </BrowserRouter>
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+      F
+    </div>
   );
 }
 
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,19 +4,110 @@ import "./Home.css";
 import Navbar from "../navbar/Navbar";
 
 function Home() {
+  const data = [
+    {
+      name: "Bohemian Rhapsody",
+      image:
+        "https://images.firstpost.com/wp-content/uploads/large_file_plugin/2018/12/1544508083_bohemianrhapsodyfeature11122018.png",
+      artist: "Queen",
+    },
+    {
+      name: "Losing My Religion",
+      image:
+        "https://cdns-images.dzcdn.net/images/cover/a968ed3814acb2ef8f3929b1bebcb42d/500x500.jpg",
+      artist: "REM",
+    },
+    {
+      name: "Beggin",
+      image:
+        "https://i1.sndcdn.com/artworks-g23acOZ1xZX7gCAr-7nTohA-t500x500.jpg",
+      artist: "Maneskin",
+    },
+    {
+      name: "Hey Jude",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Heyjudealbum.jpg/220px-Heyjudealbum.jpg",
+      artist: "The Beatles",
+    },
+    {
+      name: "Californication",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/d/df/RedHotChiliPeppersCalifornication.jpg",
+      artist: "Red Hot Chilli Peppers",
+    },
+    {
+      name: "Hey Oh",
+      image:
+        "https://i.discogs.com/Zgly92k7NjmL2JrFCfBsR0NJJ36qsWLqkPemidc2Mfo/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI5Mjky/MDEtMTMxMTg1MDUy/Ni5qcGVn.jpeg",
+      artist: "Red Hot Chilli Peppers",
+    },
+  ];
+
+  // const display = () => {
+  //   data.map(() => {
+  //     console.log(data)
+  //     // <div className="grid">
+  //     //   <img className="content-img" src={data.image} alt="" />
+  //     //   <h3>{data.name}</h3>
+  //     //   <h4>{data.artist}</h4>
+  //     // </div>;
+  //   });
+  // };
+
   return (
     <div>
       <Navbar />
       <div className="content">
-        <div>
-          <h1>Hello @User!</h1>
-        </div>
+        <h1>Hello @User!</h1>
+
         <div className="display-grid">
-          <div className="grid">
-            <p>content</p>
+          {/* {display} */}
+          <div className="header-display">
+            <h1>Discover</h1>
           </div>
-          <div className="grid"></div>
+          <div className="group-grid">
+            <div className="grid">
+              <img className="content-img" src={data[0].image} alt="" />
+              <h3>{data[0].name}</h3>
+              <h4>{data[0].artist}</h4>
+            </div>
+            <div className="grid">
+              <img className="content-img" src={data[1].image} alt="" />
+              <h3>{data[1].name}</h3>
+              <h4>{data[1].artist}</h4>
+            </div>
+            <div className="grid">
+              <img className="content-img" src={data[2].image} alt="" />
+              <h3>{data[2].name}</h3>
+              <h4>{data[2].artist}</h4>
+            </div>
+          </div>
         </div>
+
+        <div className="display-grid">
+          {/* {display} */}
+          <div className="header-display">
+            <h1>Picks for you</h1>
+          </div>
+          <div className="group-grid">
+            <div className="grid">
+              <img className="content-img" src={data[3].image} alt="" />
+              <h3>{data[3].name}</h3>
+              <h4>{data[3].artist}</h4>
+            </div>
+            <div className="grid">
+              <img className="content-img" src={data[4].image} alt="" />
+              <h3>{data[4].name}</h3>
+              <h4>{data[4].artist}</h4>
+            </div>
+            <div className="grid">
+              <img className="content-img" src={data[5].image} alt="" />
+              <h3>{data[5].name}</h3>
+              <h4>{data[5].artist}</h4>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
